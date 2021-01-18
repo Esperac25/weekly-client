5789263f22a19a601e8feebc209fe1f911f57473
Esperanza Cardenas <esperanzacardenas@Esperanzas-MBP.home>
2021-01-17 16:16:47 -0800

    updates

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,10 +7,14 @@ export default class NavBar extends Component{
   
     render(){
        return(
-           <div>
-               <Link to='/about'>About</Link>
-               <Link to='/home'>Home</Link>
-               <Link to='/items/add-item'>Add Item</Link>
+           <div className='nav'>
+           <ul className='nav-ul'>
+           <li className='link'><Link to='/about'>About</Link></li>
+           <br></br>
+           <li className='link'> <Link to='/home'>Home</Link></li>
+           <br></br>
+           <li className='link'><Link to='/items/add-item'>Add Item</Link></li>
+           </ul>
            </div>
        )
 }
