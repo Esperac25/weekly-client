0b2406c3921a6f01fad003f939e2335a9cbc7d1e
Esperanza Cardenas <esperanzacardenas@Esperanzas-MBP.home>
2021-01-19 10:24:57 -0800

    refactoring again

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -12,7 +12,7 @@ const AddItem = () => {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(body)
-      }); return response
+      }); console.log(response)
       .then(
         window.location='/home'
       )
